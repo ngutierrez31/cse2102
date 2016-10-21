943f7bbcce5a99cc169631667fc6a46357277646
Lynn <tpepin96@gmail.com>
2016-10-21 18:24:54 -0400

    Added most of csv Parse functionality; now need to use Google Maps API to get the zipcode.

--- src/foodlocator/locationParser.java
+++ src/foodlocator/locationParser.java
@@ -1,5 +1,7 @@
 package foodlocator;
 
+import java.util.Scanner;
+
 public class locationParser {
 
 	public static LocationObject txtParser(String lineIn){
@@ -10,23 +12,40 @@ public class locationParser {
 	}
 	
 	public static LocationObject csvParser(String lineIn){
-		// TODO: Content
+		// TODO: Test if this parses right, returns a location right!
 		// The below lines are so Eclipse doesn't complete. (Won't compile if this doesn't return a Location
 		
-		String 	long_in; 	// Geocode orders are lat/long, but we get long/lat first. ew.
-		String 	lati_in;
-		String 	name_in;
-		
-		float 	longitude;
+		float 	longitude;	// Geocode orders are lat/long, but we get long/lat first.
 		float 	latitude;		
 		String 	name;		//E.g. McDonalds
-		int		zipcode;
+		int		zipcode;	// Get THIS from GOOGLE
 		int 	type;		// 0 is healthy, 1 is junkfood
 		String	address;	// E.g 3828 W Dimond Blvd, Anchorage,AK
 		String	phone;
+		type = 1;		// Junk foods are in csv files
+		
+		// Example string: -149.95038,61.13712,"McDonalds-Anchorage,AK","3828 W Dimond Blvd, Anchorage,AK, (907) 248-0597"
+		// Delimited string: -149.95038, 61.13712, McDonalds, Anchorage, AK, 3828 W Dimond Blvd, Anchorage, AK, (907) 248-0597
+		// Useful site: https://regex101.com/
+		Scanner sc = new Scanner(lineIn).useDelimiter(",|\"|-"); 	//Delimits by the following symbols: , " -
+		longitude = Float.parseFloat(sc.next());
+		latitude = 	Float.parseFloat(sc.next());
+		name = sc.next();
+		sc.next(); sc.next();
+		address = 	sc.next() + ", " + sc.next() + ", " + sc.next();
+		phone = 	sc.next() + " " + sc.next() + "-" + sc.next();
+		
+		// TODO : GET ZIPCODE USING GOOGLE
+		zipcode = 00000; //Temporary
 		
 		
-		LocationObject result = new LocationObject();
+		LocationObject result = new LocationObject(name, latitude, longitude, zipcode, address, type, -1, phone); // -1 is distance; change upon search
 		return result;
 	}
+
+/*
+public LocationObject(	String storeName, float storeLat, float storeLong, int storeZipcode,
+String storeStreetAddress, int storeType, float storeDistance, String storePhone) {
+	}
+*/
 }
